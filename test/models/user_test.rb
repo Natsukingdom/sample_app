e4fe3f3080f441c7a96aed7223960181d0d8fae1
NatsukiMaehara <natsuki.m.1992@gmail.com>
2018-06-18 23:03:48 +0900

    6.2.5 まで 完了

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
   def setup
     @user = User.new(name: 'Example User', email: 'user@example.com')
   end
@@ -30,4 +29,35 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test 'email validation should accept valid addresses' do
+    valid_adresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                        first.last@foo.jp aclice+bob@baz.cn]
+    valid_adresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid? "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test 'email validation should reject invalid addresses' do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com foo@bar_baz..com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test 'email addresses should be unique' do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test 'email addresses should be saved as lower-case' do
+    mixed_case_email = 'FoO@eXamPle.com'
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
